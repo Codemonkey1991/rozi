631ea53e541e4bd0ab4f8b85c14f72eeffee0f38
Tomas Sandven <tomas191191@gmail.com>
2014-11-07 17:33:50 +0100

    Switched from load to require for lib files

--- lib/rozi.rb
+++ lib/rozi.rb
@@ -7,14 +7,14 @@ module Rozi
   # Loads all ruby files under lib/rozi. Called automatically when requiring
   # "rozi.rb".
   #
-  def self.load_lib
+  def self.require_lib!
     this_dir = File.absolute_path(File.dirname(__FILE__))
     source_files = Dir[File.join(this_dir, "rozi/**/*.rb")]
 
     source_files.each { |file|
-      load(file)
+      require_relative file
     }
   end
 end
 
-Rozi.load_lib()
+Rozi.require_lib!()
