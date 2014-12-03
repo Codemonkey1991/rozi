ce1ea27043081ec2c3cae74b44b75095d1b80c6a
Tomas Sandven <tomas191191@gmail.com>
2014-12-03 08:33:30 +0100

    Updated module_functions_test.rb
    Removed an old test and updated existing tests to use helper methods
    defined in RoziTestSuite.

--- test/rozi/module_functions_test.rb
+++ test/rozi/module_functions_test.rb
@@ -1,11 +1,10 @@
 
 require "tempfile"
 
-module Rozi
+module RoziTestSuite
   class ModuleFunctionsTest < Minitest::Test
-
     def test_open_file
-      temp_file_path { |file_path|
+      RoziTestSuite.temp_file_path { |file_path|
         file = Rozi.open_file(file_path, "w")
         file.write("foo\nbar\næøå")
         file.close
@@ -17,7 +16,7 @@ module Rozi
     end
 
     def test_open_file_with_block
-      temp_file_path { |file_path|
+      RoziTestSuite.temp_file_path { |file_path|
         f = nil
 
         Rozi.open_file(file_path, "w") { |file|
@@ -29,46 +28,5 @@ module Rozi
         assert f.closed?
       }
     end
-
-    # def test_write_track
-    #   track = Rozi::Track.new()
-    #   track << Rozi::TrackPoint.new(latitude: 59.91273, longitude: 10.74609)
-    #   track << Rozi::TrackPoint.new(latitude: 60.39358, longitude: 5.32476)
-    #   track << Rozi::TrackPoint.new(latitude: 62.56749, longitude: 7.68709)
-
-    #   temp_file_path { |file_path|
-    #     Rozi.write_track(track, file_path)
-
-    #     text = File.read(file_path, mode: "rb")
-    #     expected_output = read_test_file("expected_output_1.plt")
-
-    #     assert_equal expected_output, text
-    #   }
-    # end
-
-    private
-
-    def read_test_file(name)
-      path = File.join(Rozi::ROOT, "test/test_data", name)
-
-      File.read(path, mode: "rb")
-    end
-
-    def temp_file_path(name="temp", suffix="")
-      path = Dir::Tmpname.make_tmpname(
-        "#{Dir::Tmpname.tmpdir}/rozi", "#{name}#{suffix}"
-      )
-
-      if block_given?
-        begin
-          yield path
-        ensure
-          File.unlink path if File.exist? path
-        end
-      else
-        return path
-      end
-    end
-
   end
 end
